c8a4a47a1e4fa513e6f47b90ed246bb2ec33fe0d
Beatriz Borges <beatrizmbribeiro@gmail.com>
2019-12-18 09:20:06 +0100

    minor prettifications to the website ✨

--- docs/_site/assets/custom.js
+++ docs/_site/assets/custom.js
@@ -1,7 +1,22 @@
 $(document).ready(function(){
-    $('h2 a').click(function() {
+
+    $('h2 a').click(function(e) {
+        // caret change
         i = $(this).find('i')
         i.toggleClass('fa-caret-right');
         i.toggleClass('fa-caret-down');
+    });
+
+    $('ul.sticky a').click(function(e) {
+
+        console.log('here')
+
+        // scrolling
+        var url = e.target.href;
+        var hash = url.substring(url.indexOf("#")+1);
+        $('html, body').animate({
+            scrollTop: $('#'+hash).offset().top
+        }, 500);
+        return false;
     });
 });
